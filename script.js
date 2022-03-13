44566d4ec5820b6db6d6afff27960536a9d0d7ae
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-03-13 22:58:38 +0300

    adding functionality

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 $('#validate').submit(function(e){
-    e.preventDefault(); 
-    var mail = $('#email').val().toLowercase();
+    e.preventDefault;
+    var mail = $('#email').val().toLowerCase();
+    if(validateEmail(mail)!== true){
+        alert('Enter a valid email')
+    }
+    
 })
-function validateemail(email){
-    let regex= /^[a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+@[a-zA-Z0-9-]+(?:\. [a-zA-Z0-9-]+)*$/;
+
+function validateEmail(email){
+    let regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if(email.match(regex)){
         return (true);
-    } else{
+    } else {
         return (false);
     }
-};
+  
+}
+
+var name= $('#name').val();
+  if(validateName(user)!==true){
+    alert("Name must be alphabets")
+  }
+
+function validateName(user){
+    let nameRegex = /^[a-zA-Z]*$/;
+    if(nameRegex.test(user)){
+        return (true);
+    } else {
+        return (false);
+    }
+}
+
+
+    
+
